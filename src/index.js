73972c4d0d771bbe56fc5bf1eff0cfc1b7dc5ced
Lucas Araujo <araujolucas994@gmail.com>
2022-10-17 13:45:19 -0300

    new file:   src/components/Button.css 	new file:   src/components/Button.jsx 	new file:   src/components/Display.css 	new file:   src/components/Display.jsx 	new file:   src/fonts/RobotoMono-Thin.ttf 	modified:   src/index.css 	modified:   src/index.js 	new file:   src/main/Calculator.css 	new file:   src/main/Calculator.jsx

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <>
+  <h1>Calculadora</h1>
+  <Calculator />
+  </>,
   <React.StrictMode>
-    <App />
   </React.StrictMode>
 );
 
